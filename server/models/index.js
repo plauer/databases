d8265afd872ea925fd5cc732a4e323edf64aae30
plauer <plauer411@gmail.com>
2014-11-25 11:20:50 -0700

    installs node dependencies in root and client dependencies via bower in client folder. creates database

--- server/models/index.js
+++ server/models/index.js
@@ -5,8 +5,12 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function () {
+      var queryString = ""
+    }, // a function which produces all the messages
+    post: function () {
+      var queryString = ""
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
--- server/schema.sql
+++ server/schema.sql
@@ -3,9 +3,18 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id int NOT NULL AUTO_INCREMENT,
+  userid int NOT NULL,
+  text varchar(200) NOT NULL,
+  roomname varchar (20),
+  PRIMARY KEY (ID)
 );
 
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT,
+  username varchar(40) NOT NULL,
+  PRIMARY KEY (id)
+)
 /* Create other tables and define schemas for them here! */
 
 
